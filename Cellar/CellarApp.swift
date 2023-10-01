0885426a5b96710b735e65a1028daa7bb3a2cc51
dcas796 <89193854+dcas796@users.noreply.github.com>
2023-10-01 13:45:22 +0200

    Initial Commit

---
+++ Cellar/CellarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CellarApp.swift
+//  Cellar
+//
+//  Created by Dani on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CellarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
